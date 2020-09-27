9c5907dcb78a69a2f401f4952dff1f36161750c5
Michelle Lu <ml1417@crayon.cs.rutgers.edu>
2020-09-27 01:39:35 -0400

    some error checking

--- First_Project/PartialHTTP1Server.java
+++ First_Project/PartialHTTP1Server.java
@@ -1,7 +1,11 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+//import java.io.IOException;
 
-
+/*
 public class HelloThread extends Thread {
 
     public void run() {
@@ -13,23 +17,81 @@ public class HelloThread extends Thread {
     }
 
 }
-class PartialHTTP1Server {
+*/
+/*
+class client_handler extends Thread
+{
+    final DataInputStream dis; 
+    final DataOutputStream dos; 
+    final Socket s;
 
-    public static void main(String args[]) throws Exception {
-        if (args.length != 1) {
+    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos)  
+    { 
+        this.s = s; 
+        this.dis = dis; 
+        this.dos = dos; 
+    }
+
+    public void run()  
+    {  
+        while (true)  
+        { 
+            System.out.println("hi!");
+        }   
+
+}
+
+
+
+class PartialHTTP1Server 
+{
+
+    public static void main(String args[]) throws Exception 
+    {
+        if (args.length != 1) 
+        {
             System.err.println("Usage: java PartialHTTP1Server <port number>");
             System.exit(1);
         }
-    
+        //create server socket given port number
         int portNumber = Integer.parseInt(args[0]);
         ServerSocket serverSocket = new ServerSocket(portNumber);
 
-        while (true) {
+        while (true) 
+        {
             Socket connectionSocket = serverSocket.accept();
             
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
+            clientSentence = inFromClient.readLine();
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence); 
+
+            //Thread t = new client_handler(serverSocket, inFromClient, outToClient);
+            //t.start();
+
         }
     }
 }
+*/
+
+class PartialHTTP1Server 
+{
+ public static void main(String argv[]) throws Exception
+ {
+    String clientSentence;
+    String capitalizedSentence;
+    ServerSocket welcomeSocket = new ServerSocket(6789);
+
+    while(true) 
+    {
+        Socket connectionSocket = welcomeSocket.accept();
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+        clientSentence = inFromClient.readLine();
+        capitalizedSentence = clientSentence.toUpperCase() + '\n';
+        outToClient.writeBytes(capitalizedSentence); 
+    }
+ }
+}  
